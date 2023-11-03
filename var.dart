20a98245ac2b72345ac59ff68034bebb474ebab2
CodeByDehati <149818315+CodeByDehati@users.noreply.github.com>
2023-11-03 22:28:25 +0530

    Add files via upload

---
+++ var.dart
@@ -0,0 +1,43 @@
+void main() {
+
+  //Multiple Values
+  // int i, j, k;
+
+  // i = 10;
+  // j = 20;
+  // k = 30;
+
+  
+// var and  dynamic
+
+  // var a = 10;
+  // var b = "aditya";
+
+  // a = a + 10;
+
+  // dynamic a = 10;
+
+  // a = a + 10;
+
+  // a = 10.0;
+
+  // print(a);
+
+//  final and const keyword
+
+  // final a = 10;
+
+  // print(a);
+
+  // const a = 10;
+
+  // print(a);
+
+  // var a;
+  // print(a);
+
+  // final b;
+  // print(b);
+  
+
+}
